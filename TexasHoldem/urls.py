5dc62d4791cf5cad1561c1271f9554d26a06aff3
lv.weichao <lv.weichao@yottabyte.cn>
2019-02-19 01:02:04 +0800

    [improvement] 引入tastypie，接口符合restful

--- TexasHoldem/urls.py
+++ TexasHoldem/urls.py
@@ -13,18 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from texas import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^index/$', views.index),
-    url('^join/$', views.join_game),
-    url('^start/$', views.start_game),
-    url('^check/$', views.check),
-    url('^call/$', views.call),
-    url('^raise/$', views.raise_bet),
-    url('^fold/$', views.fold),
-    url('^info/$', views.info),
+    url(r'^', include('texas.urls'))
 ]
